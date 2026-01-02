ef4a02e140932f56375556b9fc7ae0ba8d2e41cf
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-02 16:08:46 +0700

    menambahkan function HostNavigasi

--- app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
@@ -15,3 +15,22 @@ import com.example.myfirebase.view.EntrySiswaScreen
 fun DataSiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier) {
     HostNavigasi(navController = navController)
 }
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = modifier ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")
+                })
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.navigate(DestinasiHome.route) })
+        }
+    }
+}
