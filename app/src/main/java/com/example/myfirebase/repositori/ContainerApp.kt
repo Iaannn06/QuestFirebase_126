7de1f3b9bf159c0c02c0cfa14b9bb6fb70f731f0
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-02 15:49:44 +0700

    menambahkan function getAllSiswa

--- app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/myfirebase/repositori/ContainerApp.kt
@@ -2,3 +2,7 @@ package com.example.myfirebase.repositori
 
 import com.example.myfirebase.modeldata.Siswa
 import kotlinx.coroutines.flow.Flow
+
+interface RepositorySiswa {
+    fun getAllSiswa(): Flow<List<Siswa>>
+}
