05a053164d73dc9fc0aad9a1f9ca61750df63b06
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-02 15:55:17 +0700

    menambahkan interface RepositorySiswa.kt

--- app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
+++ app/src/main/java/com/example/myfirebase/repositori/RepositorySiswa.kt
@@ -4,3 +4,8 @@ import com.example.myfirebase.modeldata.Siswa
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 
+interface RepositorySiswa {
+    suspend fun getDataSiswa(): List<Siswa>
+    suspend fun postDataSiswa(siswa: Siswa)
+}
+
