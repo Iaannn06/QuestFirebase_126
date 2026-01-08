9f3dbc9780483781bce540327127076064a35a67
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-08 20:08:04 +0700

    penyesuaian EditViewModel.kt

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -1,17 +1,25 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.myfirebase.modeldata.DetailSiswa
 import com.example.myfirebase.modeldata.UIStateSiswa
+import com.example.myfirebase.modeldata.toDataSiswa
+import com.example.myfirebase.modeldata.toUiStateSiswa
 import com.example.myfirebase.repositori.RepositorySiswa
 import com.example.myfirebase.view.route.DestinasiDetail
 import kotlinx.coroutines.launch
 
-class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa:
-RepositorySiswa): ViewModel() {
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
 
@@ -22,29 +30,33 @@ RepositorySiswa): ViewModel() {
     init {
         viewModelScope.launch {
             uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
-                .toUIStateSiswa(true)
+                .toUiStateSiswa(true)
         }
     }
 
     fun updateUiState(detailSiswa: DetailSiswa) {
         uiStateSiswa =
             UIStateSiswa(
-                detailSiswa = detailSiswa, isEntryValid = validasiInput
-                    (detailSiswa)
+                detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa)
             )
     }
 
-    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa ): Boolean {
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
         return with(uiState) {
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
 
-    suspend fun editSatuSiswa(){
-        if (validasiInput(uiStateSiswa.detailSiswa)){
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
             try {
-                repositorySiswa.editSatuSiswa(idSiswa, uiStateSiswa.detailSiswa.toDataSiswa
-                    ())
+                repositorySiswa.editSatuSiswa(
+                    idSiswa,
+                    uiStateSiswa.detailSiswa.toDataSiswa()
+                )
                 println("Update Sukses: $idSiswa")
             } catch (e: Exception) {
                 println("Update Error: ${e.message}")
