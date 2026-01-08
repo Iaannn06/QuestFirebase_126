6e892f60d00763ef6d1dbc9cbae8b33a89d6150c
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-08 15:28:27 +0700

    menambahkan scaffold dan topBar

--- app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
@@ -1,8 +1,11 @@
 package com.example.myfirebase.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.example.myfirebase.view.components.SiswaTopAppBar
+import com.example.myfirebase.view.route.DestinasiEdit
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -10,7 +13,16 @@ fun editSiswaScreen(
     navigateBack: () -> Unit,
     onNavigateUp: () -> Unit,
     modifier: Modifier = Modifier,
-
     ){
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
 
+            )
+        },
+
+    )
 }
