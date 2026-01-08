48b19303bc179417800fdae50c95a0ae41a026de
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-08 15:26:46 +0700

    menambahkan isi dari function editSiswaScreenn

--- app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
@@ -2,11 +2,15 @@ package com.example.myfirebase.view
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
-@OptIn(ExperimentalMaterial3Api)
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun editSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
 
-){
+    ){
 
 }
