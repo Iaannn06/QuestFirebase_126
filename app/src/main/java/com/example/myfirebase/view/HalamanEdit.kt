ea33e5971f797f179cb4d4837aa946824ea64711
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-08 15:29:57 +0700

    menambahkan inner padding

--- app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
@@ -3,7 +3,9 @@ package com.example.myfirebase.view
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.example.myfirebase.view.components.SiswaTopAppBar
 import com.example.myfirebase.view.route.DestinasiEdit
 
@@ -23,6 +25,9 @@ fun editSiswaScreen(
 
             )
         },
+        modifier = modifier
+    ) { innerPadding ->
+        val caroutineScope = rememberCoroutineScope ()
 
-    )
+    }
 }
