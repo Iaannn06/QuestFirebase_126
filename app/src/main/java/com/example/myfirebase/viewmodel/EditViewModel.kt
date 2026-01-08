5e12477d99251ee3afbbf4c7c2d132d03b3bef7d
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-08 16:50:51 +0700

    perbaikan setelah kerusakan file

--- app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/myfirebase/viewmodel/EditViewModel.kt
@@ -1,2 +1,10 @@
 package com.example.myfirebase.viewmodel
 
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.myfirebase.repositori.RepositorySiswa
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositorySiswa: RepositorySiswa:
+RepositorySiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf
+}
