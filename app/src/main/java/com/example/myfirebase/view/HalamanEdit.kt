6b0f1894ce760c39325c201e451a106cdf1e17e9
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2026-01-08 15:25:07 +0700

    menambahkan function editSiswaScreenn

--- app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
+++ app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
@@ -1,2 +1,12 @@
 package com.example.myfirebase.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api)
+@Composable
+fun editSiswaScreen(
+
+){
+
+}
